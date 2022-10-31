1c1c17942ec07e50bef3e25240278ce591d13f50
Selwyn Polit <selwynpolit@gmail.com>
2022-10-30 21:29:01 -0500

    Modal forms example

---
+++ web/modules/custom/modal_examples/src/Controller/ModalExamplesController2.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ *
+ * Contains \Drupal\modal_examples\Controller\ModalExamplesController2.
+ */
+namespace Drupal\modal_examples\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\OpenModalDialogCommand;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormBuilder;
+
+/**
+ * ModalExamplesController2 class.
+ */
+class ModalExamplesController2 extends ControllerBase {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilder
+   */
+  protected $formBuilder;
+
+  /**
+   * The ModalFormExampleController constructor.
+   *
+   * @param \Drupal\Core\Form\FormBuilder $formBuilder
+   *   The form builder.
+   */
+  public function __construct(FormBuilder $formBuilder) {
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The Drupal service container.
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * Callback for opening the modal form.
+   */
+  public function openModalForm() {
+    $response = new AjaxResponse();
+
+    // Get the modal form using the form builder.
+    $modal_form = $this->formBuilder->getForm('Drupal\modal_examples\Form\ExampleModalForm');
+
+    // Add an AJAX command to open a modal dialog with the form as the content.
+    $response->addCommand(new OpenModalDialogCommand('My Modal Form', $modal_form, ['width' => '800']));
+
+    return $response;
+  }
+
+}
