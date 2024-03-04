2fab7928452794d2bcb013a3e8cc7f6dcb17ddce
Selwyn Polit <selwynpolit@gmail.com>
2024-03-04 11:34:07 -0600

    Update config for contributing link

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -44,7 +44,7 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: 'About', link: '/about' },
       { text: 'Attribution', link: '/attribution' },
-      { text: 'Contribution', link: '/contribution'},
+      { text: 'Contributing', link: '/contributing'},
       { text: 'Fork me', link: 'https://github.com/selwynpolit/d9book/fork' },
     ],
 
